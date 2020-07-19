d6682d5ce97b60f1e4fdfeadf990a98449686bac
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-07-19 12:27:08 -0700

    Add ScreenEditor and stubs for navigation screens

---
+++ Editor/ScreenEditor.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using System.Linq;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace VioletUI {
+	//run this class when unity opens
+	[InitializeOnLoad]
+	public class ScreenEditor {
+
+		//width of inspector buttons
+		private const float BUTTON_WIDTH = 60f;
+
+		//where to show the buttons
+		private static GUIStyle singleStyle, leftStyle, rightStyle;
+
+		//hash set of all menus in scene
+		private static HashSet<NavigationScreen> menus = new HashSet<NavigationScreen>();
+
+		//menu screens to remove
+		private static List<NavigationScreen> garbage = new List<NavigationScreen>();
+
+		//constructor
+		static ScreenEditor() {
+
+			//only show while !playing
+			if (!EditorApplication.isPlaying) {
+
+				//actual editor event functions
+				EditorApplication.hierarchyWindowItemOnGUI += DrawHierarchyItem;
+				EditorApplication.update += Update;
+
+			}
+
+		}
+
+		//remove all menu screens that are no longer used to avoid memory leak
+		private static void Clean(HashSet<NavigationScreen> menus) {
+
+			//clear the unused menus
+			garbage.Clear();
+
+
+			//add all menus that no longer exist to the garbage list
+			foreach (var menu in menus) {
+				if (menu == null) {
+					garbage.Add(menu);
+				}
+			}
+
+			//remove the garbage from the menus list
+			foreach (var menu in garbage) {
+				menus.Remove(menu);
+			}
+		}
+
+
+		//update visuals
+		private static void Update() {
+			//remove the garbage from the menu list
+			Clean(menus);
+
+			foreach (var menu in Object.FindObjectsOfType<NavigationScreen>()) {
+				menus.Add(menu);
+			}
+		}
+
+		private static GUIStyle InitStyle(ref GUIStyle style, GUIStyle original, TextAnchor alignment) {
+			if (style == null) {
+				style = new GUIStyle(original);
+				style.alignment = alignment;
+				style.richText = true;
+			}
+
+			return style;
+		}
+
+		private static bool Button(Rect rect, string label, string tooltip, Color color, FontStyle fontStyle = FontStyle.Normal, int dot = 0, int i = 0, int n = 1) {
+			rect = rect.BackfillRight(BUTTON_WIDTH);
+
+			GUIStyle style = null;
+
+			if (n == 1) {
+				style = InitStyle(ref singleStyle, EditorStyles.miniButton, TextAnchor.MiddleLeft);
+			} else if (n == 2) {
+				switch (i) {
+					case 0:
+						style = InitStyle(ref leftStyle, EditorStyles.miniButtonLeft, TextAnchor.MiddleLeft);
+						break;
+					case 1:
+						style = InitStyle(ref rightStyle, EditorStyles.miniButtonRight, TextAnchor.MiddleRight);
+						break;
+				}
+			}
+
+			var content = new GUIContent(" " + label, null, tooltip);
+
+			style.fontStyle = fontStyle;
+
+			var response = GUI.Button(rect, content, singleStyle);
+
+			style.fontStyle = FontStyle.Normal;
+
+			var shadowColor = Color.Lerp(color, Color.black, 0.5f);
+			shadowColor.a = 0.8f;
+
+			if (dot > 0) {
+				var dotRect = rect.BackfillRight(rect.height).DisplaceX(1f)/*.Pad( 4f )*/;
+				var dotColor = dot == 2 ? color : Color.grey;
+
+				dotColor.a = 0.5f;
+
+				GUI.Box(dotRect, GUIContent.none);
+				EditorGUI.DrawRect(dotRect.Pad(3f).DisplaceX(-1f), dotColor);
+
+			}
+
+			rect = rect.SetWidth(3f).DisplaceX(4f).PadVertical(1f);
+
+			EditorGUI.DrawRect(rect.ExtrudeLeft(1f), shadowColor);
+			EditorGUI.DrawRect(rect.ExtrudeRight(1f), shadowColor);
+
+			color.a = 0.5f;
+
+			EditorGUI.DrawRect(rect, color);
+
+			return response;
+		}
+
+		private static bool TryGetManagerColor(int instanceID, out Color color) {
+            color = new Color(0.898f, 0.745f, 0.935f);
+            return true;
+		}
+
+		private static void DrawHierarchyItem(int instanceID, Rect rect) {
+			var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            Color violet = new Color(0.898f, 0.745f, 0.935f);
+			float h = 0f, s = 0f, v = 0f;
+			Color.RGBToHSV(violet, out h, out s, out v);
+			Color saturatedViolet = Color.HSVToRGB(h, 1f, 1f);
+            if (gameObject == null) { return; }
+
+            if (gameObject.GetComponent<NavigationController>() != null) {
+                if (Button(rect, "Reset", "", saturatedViolet, FontStyle.Normal)) {
+									foreach (var menu in menus) {
+										menu.gameObject.SetActive(false);
+									}
+                }
+                return;
+            }
+
+            if (gameObject.GetComponent<NavigationScreen>() != null) {
+                if (Button(rect, "Edit", "", saturatedViolet, FontStyle.Normal, gameObject.activeSelf ? 2 : 1)) {
+					foreach (var menu in menus) {
+						menu.gameObject.SetActive(false);
+					}
+					gameObject.SetActive(true);
+                }
+                return;
+            }
+		}
+	}
+}
---
+++ PrefabApplier.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+/// <summary>
+/// PrefabApplier is responsible for applying changes to all child prefabs
+/// without the designer having to remember to hit the override dropdown.
+///
+/// It does this by hooking into the scene save event, so when the designer saves the scene
+/// all the prefab overrides will be automatically applied.
+/// </summary>
+[ExecuteAlways]
+public class PrefabApplier : MonoBehaviour {
+	void OnDestroy() {
+		EditorSceneManager.sceneSaved -= EditorSceneManager_sceneSaved;
+	}
+
+	// tried this in Awake and OnDestroy but apparently these get blown away
+	// when the project recompiles sooo why not here!
+	void Update() {
+		EditorSceneManager.sceneSaved -= EditorSceneManager_sceneSaved;
+		EditorSceneManager.sceneSaved += EditorSceneManager_sceneSaved;
+	}
+
+	bool isAutosaving = false;
+
+	void EditorSceneManager_sceneSaved(Scene scene) {
+		if (Application.isPlaying) {return;}
+		if (isAutosaving) {
+			isAutosaving = false;
+			return;
+		}
+		UpdatePrefabs();
+	}
+
+	[Button, GUIColor(0.898f, 0.745f, 0.935f)]
+	public void UpdatePrefabs() {
+		bool hasChanges = false;
+		foreach (Transform child in transform) {
+			if (PrefabUtility.HasPrefabInstanceAnyOverrides(child.gameObject, false)) {
+				PrefabUtility.ApplyPrefabInstance(child.gameObject, InteractionMode.AutomatedAction);
+				hasChanges = true;
+			}
+		}
+        if (!hasChanges) { return; }
+
+
+		var scene = SceneManager.GetActiveScene();
+		EditorSceneManager.MarkSceneDirty(scene);
+		isAutosaving = true;
+		EditorSceneManager.sceneSaved -= EditorSceneManager_sceneSaved;
+		EditorSceneManager.SaveScene(scene);
+		EditorSceneManager.sceneSaved += EditorSceneManager_sceneSaved;
+		isAutosaving = false;
+	}
+}
+#endif
