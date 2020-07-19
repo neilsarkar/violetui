bf358bd12b4014e289e66e22785f57737b509f70
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-07-19 12:44:14 -0700

    Unpack prefab when editing, repack when done

--- Navigation/NavigationScreen.cs
+++ Navigation/NavigationScreen.cs
@@ -1,16 +1,21 @@
+using System;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class NavigationScreen : TidyBehaviour {
-
 #if UNITY_EDITOR
 	public void StartEditing() {
-		PrefabUtility.UnpackPrefabInstance(gameObject, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+		try {
+			PrefabUtility.UnpackPrefabInstance(gameObject, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+		} catch (ArgumentException e) {
+			UnityEngine.Debug.LogWarning($"VioletUI: Couldn't unpack prefab for {name}. Make sure the NavigationScreen is a prefab. {e}");
+		}
 		gameObject.SetActive(true);
 	}
 
 	public void StopEditing() {
+		PrefabUtility.SaveAsPrefabAssetAndConnect(gameObject, $"Assets/Implementation/Menus/{name}.prefab", InteractionMode.AutomatedAction);
 		gameObject.SetActive(false);
 	}
 #endif
