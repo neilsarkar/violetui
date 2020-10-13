e94fa3623e22a5eb870e802075cb0f3f21052e59
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-10-13 08:32:57 -0700

    Remove autogenerated ScreenId from sample project

--- Runtime/Navigation/ScreenId.cs
+++ Runtime/Navigation/ScreenId.cs
@@ -1,9 +1,6 @@
-//Names are automatically added through ScreenIdGenerator.cs, deletions are done manually :)
+// Names are automatically added through ScreenIdGenerator.cs, deletions are done manually :)
 namespace VioletUI {
 	public enum ScreenId {
 		None = 0,
-		ScreenA = 1,
-		ScreenB = 2,
-		Showhide = 3,
 	}
 }
