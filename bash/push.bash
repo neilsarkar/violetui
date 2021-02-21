435ce20983a10d068e520a5cb18812af0d89e8d6
Neil Sarkar <neil.r.sarkar@gmail.com>
2021-02-21 12:44:29 -0500

    Update push script to allow multiple folders

--- bash/push.bash
+++ bash/push.bash
@@ -1,4 +1,22 @@
-cp -r ./ ~/work/King_of_the_Hat/Packages/violetui/
-rm -rf ~/work/King_of_the_Hat/Packages/violetui/.git/
-rm ~/work/King_of_the_Hat/Packages/violetui/*.bash
-rm ~/work/King_of_the_Hat/Packages/violetui/*.bash.meta
+echo "Choose a project:"
+echo "[1] king_of_the_hat"
+echo "[2] Package Party"
+printf "=> "
+read;
+
+case $REPLY in
+	1)
+		projectName="king_of_the_hat"
+		;;
+	2)
+		projectName="Package Party"
+		;;
+	*)
+		echo "You entered ${REPLY} but we looking for a number on the list"
+		exit
+		;;
+esac
+
+cp -r ./ "$HOME/work/${projectName}/Packages/violetui/"
+rm -rf "$HOME/work/${projectName}/Packages/violetui/"
+rm -rf "$HOME/work/${projectName}/Packages/violetui/bash"
